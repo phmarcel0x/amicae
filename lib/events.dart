3f86f2d4f132cdbbc6dd3b6db41bb30b97dbd4f4
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:38:35 -0500

    updates

--- lib/events.dart
+++ lib/events.dart
@@ -12,25 +12,24 @@ class _EventsPage extends State<EventsPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-
-        home: Scaffold(
-            backgroundColor: Colors.yellow,
-            body: Container(
-              margin: const EdgeInsets.all(20),
-              width: double.infinity,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text("events page goes here")
-                  ),
-
-                ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text("events page goes here")
               ),
-            )
+
+            ],
+          ),
         )
+      )
     );
   }
 }
