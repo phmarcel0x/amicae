14cb36fba4c8e0019067d3609f67c0b5be3fa485
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:38:44 -0500

    updates

--- lib/spots.dart
+++ lib/spots.dart
@@ -12,26 +12,23 @@ class _SpotsPage extends State<SpotsPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-
-        home: Scaffold(
-
-            backgroundColor: Colors.green,
-            body: Container(
-              margin: const EdgeInsets.all(20),
-              width: double.infinity,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text("spots page goes here")
-                  ),
-
-                ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text("spots page goes here")
               ),
-            )
+            ],
+          ),
         )
+      )
     );
   }
 }
