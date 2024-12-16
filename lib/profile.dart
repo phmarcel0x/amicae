b6483ee23eed0186942bc9db2c180646e8de8f27
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:38:47 -0500

    updates

--- lib/profile.dart
+++ lib/profile.dart
@@ -13,39 +13,38 @@ class _ProfilePage extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-
-        home: Scaffold(
-            backgroundColor: Colors.blue,
-            body: Container(
-              margin: const EdgeInsets.all(20),
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text("profile page goes here")
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: IconButton(
-                          icon: const Icon(Icons.arrow_back),
-                          color: Colors.black,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder:
-                                    (context) => const LandingPage()
-                                )
-                            );
-                          }
-                      )
-                  ),
-                ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Text("profile page goes here")
               ),
-            )
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder:
+                        (context) => const LandingPage()
+                      )
+                    );
+                  }
+                )
+              ),
+            ],
+          ),
         )
+      )
     );
   }
 }
