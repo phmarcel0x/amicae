b244e50d8cb85e1bbed8b3a2e076b59ceda14bf5
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:35:18 -0500

    updates

--- lib/events.dart
+++ lib/events.dart
@@ -19,10 +19,10 @@ class _EventsPage extends State<EventsPage> {
             body: Container(
               margin: const EdgeInsets.all(20),
               width: double.infinity,
-              child: Column(
+              child: const Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Padding(
+                  Padding(
                       padding: EdgeInsets.all(10),
                       child: Text("events page goes here")
                   ),
