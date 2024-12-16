43372bfd1bdd0a56c11b55e1c1a7afdc61a19f89
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:35:02 -0500

    updates

--- lib/spots.dart
+++ lib/spots.dart
@@ -20,10 +20,10 @@ class _SpotsPage extends State<SpotsPage> {
             body: Container(
               margin: const EdgeInsets.all(20),
               width: double.infinity,
-              child: Column(
+              child: const Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Padding(
+                  Padding(
                       padding: EdgeInsets.all(10),
                       child: Text("spots page goes here")
                   ),
