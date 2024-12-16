6dd72db121b44b49fc512e35a01f54c31e84afce
nathan-au <im.nathanau@gmail.com>
2024-12-16 18:38:39 -0500

    updates

--- lib/home.dart
+++ lib/home.dart
@@ -12,23 +12,22 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-
-        home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(
-          margin: const EdgeInsets.all(20),
-          width: double.infinity,
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Text("home page goes here")
-              ),
-            ],
-          ),
-        )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        width: double.infinity,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text("home page goes here")
+            ),
+          ],
+        ),
+      )
       )
     );
   }
