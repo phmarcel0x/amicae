11e3ebccef3f76ece7f0b02ab03b92fdc2e586f2
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2025-01-03 16:45:46 -0500

    added the constructor for the mentor class

---
+++ lib/user_modules/mentor.dart
@@ -0,0 +1,20 @@
+// Importation of the abstract class 
+  // to have the parent attribute and methods 
+import "../user_modules/user.dart";
+
+// importing the custom exception module
+import "../exception_modules/custom_exceptions.dart";
+
+
+// Class for the Mentor
+class Mentor extends User {
+  // Admin could start with a special character to differenciate it
+  // from the regular users in the database (like the students 
+  // and the mentors)
+  // All these would be protected attributes 
+  
+  // Admin constructor
+  Mentor(String username, String password, {required String firstName, String? middleName, required String lastName, required String email})
+      : super(username, password, firstName: firstName, middleName: middleName, lastName: lastName, email: email);
+
+}
