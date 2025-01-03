de56560386019cf9ff8057f47e59998699413446
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2025-01-03 18:59:35 -0500

    modified the toString() method

--- lib/user_modules/mentor.dart
+++ lib/user_modules/mentor.dart
@@ -13,8 +13,37 @@ class Mentor extends User {
   // and the mentors)
   // All these would be protected attributes 
   
-  // Admin constructor
-  Mentor(String username, String password, {required String firstName, String? middleName, required String lastName, required String email})
-      : super(username, password, firstName: firstName, middleName: middleName, lastName: lastName, email: email);
+  // added attributes for the mentor 
+  String company;
+  int startYear;
+  // mentees list 
+  List? menteesList;  
 
+
+  // Admin constructor
+  Mentor(String username, String password, {required String firstName, String? middleName, 
+        required String lastName, required String email, required this.company, 
+        required this.startYear, this.menteesList})
+      : super(username, password, firstName: firstName, middleName: middleName, 
+        lastName: lastName, email: email);
+
+  @override
+  String toString(){
+    String? parentString = super.toString();
+    return "$parentString You work at ${this.company} since ${this.startYear}.";
+    }
+
+  // maybe add a sort of CV like on LinkedIn 
+}
+
+main() {
+  Mentor mentor = Mentor("mentor_username", 
+  "mentor_password", 
+  firstName: "Natasha", 
+  lastName: "Dorcainvil", 
+  email: "natasha.dorcainvil@amicae.dev", 
+  company: "Amicae Corp", 
+  startYear: 2024);
+
+  print(mentor);
 }
