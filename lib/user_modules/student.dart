fa02916f97c0fd61a8d623ffc97f2f92d2cfd4e9
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2025-01-03 19:34:39 -0500

    modified the toString method

--- lib/user_modules/student.dart
+++ lib/user_modules/student.dart
@@ -5,18 +5,43 @@ import "../user_modules/user.dart";
 
 // Class for the Admin
 class Student extends User {
+  // Additionnal attributes 
+  int? studentID;
+  String? collegeUniversity;
+  int? currentYear; //First, second, third, fourth, etc.
+
+
   // Admin could start with a special character to differenciate it
   // from the regular users in the database (like the students 
   // and the mentors)
   // All these would be protected attributes 
   
   // Admin constructor
-  Student(String username, String password, {required String firstName, String? middleName, required String lastName, required String email})
-      : super(username, password, firstName: firstName, middleName: middleName, lastName: lastName, email: email);
+  Student(String username, String password, {required String firstName, 
+          String? middleName, required String lastName, 
+          required String email, required this.studentID,
+          required this.collegeUniversity, required this.currentYear})
+      : super(username, password, firstName: firstName, 
+              middleName: middleName, lastName: lastName, email: email);
 
+  @override
+  String toString() {
+    String? parentString = super.toString();
+    if (currentYear == 1){
+      return "$parentString Your student ID is $studentID and you study at $collegeUniversity. You are in your ${currentYear}st";
+      }
+    
+    else if (currentYear == 2) {
+      return "$parentString Your student ID is $studentID and you study at $collegeUniversity. You are in your ${currentYear}nd";
+      }
+    
+    else if (currentYear == 3) {
+      return "$parentString Your student ID is $studentID and you study at $collegeUniversity. You are in your ${currentYear}rd";
+      }
+    
+    else {
+      return "$parentString Your student ID is $studentID and you study at $collegeUniversity. You are in your ${currentYear}th";
+      }
+    }
+  }
 
-  int? studentID;
-  String? collegeUniversity;
-  int? currentYear; //First, second, third, fourth, etc.
-
-}
