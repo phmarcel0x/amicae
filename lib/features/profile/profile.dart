34d4249ae19d432324c9653851e328af27a98228
nathan-au <im.nathanau@gmail.com>
2025-01-28 13:14:27 -0500

    updates

--- lib/features/profile/profile.dart
+++ lib/features/profile/profile.dart
@@ -39,7 +39,7 @@ class _ProfilePage extends State<ProfilePage> {
                       )
                     );
                   }
-                )
+                ),
               ),
             ],
           ),
