37b6373655ab5fc52649e1921e874383c4741277
Doan Gia Huy Vu (Adam) <123711415+DoanGiaHuyVu@users.noreply.github.com>
2025-02-25 16:54:50 -0500

    Delete lib/features directory

--- lib/features/user_modules/main.dart
+++
@@ -1,8 +0,0 @@
-import "admin.dart";
-main(){
-  Admin newAdmin = Admin("admin_username", "admin_password", firstName: "Soeuchelle", lastName: "Michel", email: "soeuchelle.michel@amicae.com");
-  print(newAdmin);
-
-  newAdmin.firstName = "Soeuchelle";
-  print(newAdmin);
-  }
--- lib/features/user_modules/user_errors.dart
+++
@@ -1,8 +0,0 @@
-enum UserErrors {
-  maxChangesExceeded(
-      "The limit of changes permitted to change the parameter has been reached.");
-
-  const UserErrors(this.message);
-
-  final String message;
-}
