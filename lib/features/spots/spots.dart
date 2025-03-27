bb14e7e66d2216c88906486e0af31bf25d1cc087
nathan-au <im.nathanau@gmail.com>
2025-03-26 23:16:50 -0400

    added markers

--- lib/features/spots/spots.dart
+++ lib/features/spots/spots.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -11,32 +10,68 @@ class SpotsPage extends StatefulWidget {
 }
 
 class _SpotsPage extends State<SpotsPage> {
+  // List of marker data (latitude, longitude, icon size, and color)
+  final List<Map<String, dynamic>> markersData = [
+    {
+      'lat': 45.496887587705764,
+      'lng': -73.57787953861875,
+      'icon': Icons.location_on,
+      'color': Colors.red,
+    },
+    {
+      'lat': 45.497059,
+      'lng': -73.578421,
+      'icon': Icons.location_pin,
+      'color': Colors.blue,
+    },
+    {
+      'lat': 45.497059,
+      'lng': -73.578421,
+      'icon': Icons.location_on,
+      'color': Colors.green,
+    },
+    {
+      'lat': 45.497059,
+      'lng': -73.578421,
+      'icon': Icons.location_pin,
+      'color': Colors.purple,
+    },
+    // Add more markers here...
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(
-            45.497059, -73.578421), // Center the map over Concordia University
-        initialZoom: 16,
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: const MapOptions(
+              initialCenter: LatLng(45.497059, -73.578421),
+              initialZoom: 16,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: markersData.map((markerData) {
+                  return Marker(
+                    point: LatLng(markerData['lat'], markerData['lng']),
+                    width: 80,
+                    height: 80,
+                    child: Icon(
+                      markerData['icon'],
+                      size: 40,
+                      color: markerData['color'],
+                    ),
+                  );
+                }).toList(),  // Convert the generated markers to a list
+              ),
+            ],
+          ),
+        ],
       ),
-      children: [
-        TileLayer(
-          // Display map tiles from any source
-          urlTemplate:
-              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
-          userAgentPackageName: 'com.example.app',
-          // And many more recommended properties!
-        ),
-        // RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
-        //   attributions: [
-        //     TextSourceAttribution(
-        //       'OpenStreetMap contributors',
-        //       // onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
-        //     ),
-        //     // Also add images...
-        //   ],
-        // ),
-      ],
     );
   }
 }
