8f4f3962a7434468a3d5888a566cb74680e3113c
adamvu <vudoangiahuy@gmail.com>
2025-03-27 13:48:18 -0400

    Grab UID in profile.dart

--- lib/features/profile/profile.dart
+++ lib/features/profile/profile.dart
@@ -1,3 +1,72 @@
+// import 'package:amicae/features/login/pages/landing_page.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+//
+// import '../nav bar/nav_bar.dart';
+//
+// class ProfilePage extends StatelessWidget {
+//   const ProfilePage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.blue,
+//       body: Container(
+//         margin: const EdgeInsets.all(20),
+//         width: double.infinity,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Padding(
+//                 padding: EdgeInsets.all(10),
+//                 child: Text("profile page goes here")),
+//             Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: IconButton(
+//                 icon: const Icon(Icons.arrow_back),
+//                 color: Colors.black,
+//                 onPressed: () {
+//                   Navigator.pushReplacement(
+//                     context,
+//                     PageRouteBuilder(
+//                       pageBuilder: (context, animation1, animation2) =>
+//                           const NavBar(),
+//                       transitionDuration: Duration.zero,
+//                       reverseTransitionDuration: Duration.zero,
+//                     ),
+//                   );
+//                   // Navigator.push(
+//                   //   context,
+//                   //   MaterialPageRoute(builder:
+//                   //     (context) => const NavBar()
+//                   //   )
+//                   // );
+//                 },
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: IconButton(
+//                 icon: const Icon(Icons.logout),
+//                 color: Colors.black,
+//                 onPressed: () {
+//                   // TODO: firebase logout
+//                   FirebaseAuth.instance.signOut();
+//                   Navigator.of(context).pushAndRemoveUntil(
+//                     MaterialPageRoute(
+//                       builder: (context) => const LandingPage(),
+//                     ),
+//                     (route) => false,
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:amicae/features/login/pages/landing_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +78,9 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the current user
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Container(
@@ -17,6 +89,18 @@ class ProfilePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Display user ID if a user is logged in
+            if (currentUser != null)
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  "User ID: ${currentUser.uid}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
             const Padding(
                 padding: EdgeInsets.all(10),
                 child: Text("profile page goes here")),
@@ -30,17 +114,11 @@ class ProfilePage extends StatelessWidget {
                     context,
                     PageRouteBuilder(
                       pageBuilder: (context, animation1, animation2) =>
-                          const NavBar(),
+                      const NavBar(),
                       transitionDuration: Duration.zero,
                       reverseTransitionDuration: Duration.zero,
                     ),
                   );
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder:
-                  //     (context) => const NavBar()
-                  //   )
-                  // );
                 },
               ),
             ),
@@ -50,13 +128,12 @@ class ProfilePage extends StatelessWidget {
                 icon: const Icon(Icons.logout),
                 color: Colors.black,
                 onPressed: () {
-                  // TODO: firebase logout
                   FirebaseAuth.instance.signOut();
                   Navigator.of(context).pushAndRemoveUntil(
                     MaterialPageRoute(
                       builder: (context) => const LandingPage(),
                     ),
-                    (route) => false,
+                        (route) => false,
                   );
                 },
               ),
