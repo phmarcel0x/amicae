531aacebfe32eeafd12ccd9bf514e65f5b3b0424
nathan-au <im.nathanau@gmail.com>
2025-04-03 17:02:19 -0400

    fix stop back swiping to profile creation

--- lib/features/nav bar/nav_bar.dart
+++ lib/features/nav bar/nav_bar.dart
@@ -30,69 +30,76 @@ class _NavBarState extends State<NavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(automaticallyImplyLeading: false,
-    forceMaterialTransparency: true,
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 200),
-          child: Image.asset(
-            "assets/ae_short_white.png",
-            fit: BoxFit.contain,
-            height: 70,
+    return WillPopScope(
+      // This will intercept the back press or swipe back gesture
+      onWillPop: () async {
+        return false; // Disable backswiping and hardware back button
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          forceMaterialTransparency: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 200),
+                child: Image.asset(
+                  "assets/ae_short_white.png",
+                  fit: BoxFit.contain,
+                  height: 70,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: IconButton(
+                  icon: const Icon(Icons.account_circle),
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfilePage()));
+                  },
+                ),
+              ),
+            ],
           ),
+          shape: const Border(bottom: BorderSide(color: Colors.black, width: 4)),
         ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: IconButton(
-              icon: const Icon(Icons.account_circle),
-              color: Colors.black,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()));
-              }),
-        ),
-      ],
-    ),
-    shape: const Border(bottom: BorderSide(color: Colors.black, width: 4)),
-          ),
-          body: pages[selectedIndex],
-          bottomNavigationBar: BottomNavigationBar(
-    showSelectedLabels: false,
-    showUnselectedLabels: false,
-    selectedFontSize: 15,
-    selectedItemColor: Colors.orange,
-    selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-    unselectedItemColor: Colors.black,
-    // showSelectedLabels: false,
-    // showUnselectedLabels: false,
-    backgroundColor: Colors.white,
-    type: BottomNavigationBarType.fixed,
-
-    items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: "Home",
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.people),
-        label: "People",
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.event),
-        label: "Events",
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.coffee),
-        label: "Spots",
-      ),
-    ],
-    currentIndex: selectedIndex,
-    onTap: onItemTapped,
-          ),
+        body: pages[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedFontSize: 15,
+          selectedItemColor: Colors.orange,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          unselectedItemColor: Colors.black,
           backgroundColor: Colors.white,
-        );
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: "People",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.event),
+              label: "Events",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.coffee),
+              label: "Spots",
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
